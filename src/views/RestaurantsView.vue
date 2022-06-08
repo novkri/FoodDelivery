b11506ca6864acf5e9988b80e441e4544b945e23
novickay <novickaya@sputnikfund.ru>
2022-06-08 17:37:53 +0700

    add sidebar in teleport

--- src/views/RestaurantsView.vue
+++ src/views/RestaurantsView.vue
@@ -14,15 +14,17 @@
     </button>
   </div>
   <!--  The target element cannot be a part of the same component... It needs to be outside of <div id="app">. -->
-  <Teleport to="body"
-    ><PopupWindow
-      v-if="teleportOpen"
-      @confirm="teleportOpen = false"
-      @decline="teleportOpen = false"
-    >
-      <template #header>I'm from Restaurants Page</template>
-      <template #body> Teleport Test </template>
-    </PopupWindow>
+  <Teleport to="body">
+    <!--    <PopupWindow-->
+    <!--      v-if="teleportOpen"-->
+    <!--      @confirm="teleportOpen = false"-->
+    <!--      @decline="teleportOpen = false"-->
+    <!--    >-->
+    <!--      <template #header>I'm from Restaurants Page</template>-->
+    <!--      <template #body> Teleport Test </template>-->
+    <!--    </PopupWindow>-->
+
+    <ShoppingCart v-if="teleportOpen" />
   </Teleport>
 </template>
 
@@ -31,6 +33,7 @@ import RestaurantList from "@/components/Restaurant/RestaurantList.vue";
 import SpinnerLoader from "@/components/SpinnerLoader.vue";
 import { ref } from "vue";
 import PopupWindow from "@/components/PopupWindow.vue";
+import ShoppingCart from "@/components/ShoppingCart.vue";
 
 const teleportOpen = ref(false);
 </script>
