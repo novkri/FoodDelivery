3c4cdeb09512aebba37196264d0c7847de48c5f3
novickay <novickaya@sputnikfund.ru>
2022-06-08 10:39:52 +0700

    add Teleport

--- src/views/RestaurantsView.vue
+++ src/views/RestaurantsView.vue
@@ -6,11 +6,23 @@
 
     <RestaurantList />
   </Suspense>
+
+  <br />
+  <div>
+    <button @click="teleportOpen = !teleportOpen">
+      Teleport is {{ teleportOpen ? "open" : "closed" }}
+    </button>
+  </div>
+  <Teleport to="#app"><ShoppingCart v-if="teleportOpen" /></Teleport>
 </template>
 
 <script setup lang="ts">
 import RestaurantList from "@/components/Restaurant/RestaurantList.vue";
 import SpinnerLoader from "@/components/SpinnerLoader.vue";
+import ShoppingCart from "@/components/ShoppingCart.vue";
+import { ref } from "vue";
+
+const teleportOpen = ref(false);
 </script>
 
 <style scoped lang="scss"></style>
